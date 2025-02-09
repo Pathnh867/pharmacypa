ebad783942a09a11c8186af653b47742cf1e0303
Pathnh867 <117632663+Pathnh867@users.noreply.github.com>
2025-02-10 03:38:05 +0700

    updatenua02

--- pharmacyai/src/services/UserService.js
+++ pharmacyai/src/services/UserService.js
@@ -1,15 +1,27 @@
 import axios from "axios"
 
-export const axiosJWT = axios.create()
+export const axiosJWT = axios.create({
+    withCredentials: true, // Đặt mặc định để tất cả request gửi cookie
+});
 
 export const loginUser = async (data) => {
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/sign-in`, data,  withCredentials: true)
-    return res.data
-}
+    const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/sign-in`,
+        data,
+        { withCredentials: true } // Quan trọng để gửi cookie
+    );
+    return res.data;
+};
+
 export const signupUser = async (data) => {
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/sign-up`, data,  withCredentials: true)
-    return res.data
-}
+    const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/sign-up`,
+        data,
+        { withCredentials: true }
+    );
+    return res.data;
+};
+
 export const getDetailsUser = async (id, access_token) => {
     const res = await axiosJWT.get(`${process.env.REACT_APP_API_URL}/user/get-details/${id}`, {
         headers: {
@@ -19,13 +31,17 @@ export const getDetailsUser = async (id, access_token) => {
     return res.data
 }
 export const refreshToken = async (refreshToken) => {
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/refresh-token`, {}, {
-        headers: {
-            token: `Bearer ${refreshToken}`,
+    const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/refresh-token`,
+        {},
+        {
+            headers: { token: `Bearer ${refreshToken}` },
+            withCredentials: true, // Đảm bảo request này cũng gửi cookie
         }
-    })
-    return res.data
-}
+    );
+    return res.data;
+};
+
 export const logoutUser = async () => {
     const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/log-out`)
     return res.data
