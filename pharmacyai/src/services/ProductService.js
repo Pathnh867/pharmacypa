6036dfb95c13d35a01e2957f6d4e15fed6187205
Pathnh867 <117632663+Pathnh867@users.noreply.github.com>
2025-03-17 02:10:03 +0700

    fix getAll

--- pharmacyai/src/services/ProductService.js
+++ pharmacyai/src/services/ProductService.js
@@ -4,16 +4,16 @@ import { axiosJWT } from "./UserService"
 export const getAllProduct = async (search, limit) => {
     let res = {}
     if (search?.length > 0) {
-        res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-All?filter=name&filter=${search}&limit=${limit}`)
+        res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all?filter=name&filter=${search}&limit=${limit}`)
     } else {
-        res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-All?limit=${limit}`)
+        res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all?limit=${limit}`)
     }
     return res.data
 }
 
 export const getProductType = async (type, page, limit) => {
     if (type) {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-All?filter=type&filter=${type}&limit=${limit}&page=${page}`)
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all?filter=type&filter=${type}&limit=${limit}&page=${page}`)
         return res.data
     } 
 }
