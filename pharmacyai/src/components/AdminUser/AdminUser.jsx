c9e1787998ff88b532324bd5bd407f2a4fa5b3ef
Pathnh867 <117632663+Pathnh867@users.noreply.github.com>
2025-03-24 16:23:59 +0700

    Update AdminUser.jsx

--- pharmacyai/src/components/AdminUser/AdminUser.jsx
+++ pharmacyai/src/components/AdminUser/AdminUser.jsx
@@ -102,7 +102,7 @@ const AdminUser = () => {
   // Mutations
   const createMutation = useMutationHooks(
     (data) => {
-      return UserService.createUser(data.userData, data.token);
+      return UserService.create(data.userData, data.token);
     }
   );
   
