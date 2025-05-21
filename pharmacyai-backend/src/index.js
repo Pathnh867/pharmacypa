e9688ac5342da3133a432bc3f88fb1000a8a26a6
Pathnh867 <117632663+Pathnh867@users.noreply.github.com>
2025-05-21 21:14:07 +0700

    trở về local

--- pharmacyai-backend/src/index.js
+++ pharmacyai-backend/src/index.js
@@ -9,15 +9,23 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3001
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://pharmacypa.vercel.app"
+];
 
 app.use(
-    cors({
-      origin: "https://pharmacypa.vercel.app", // Chỉ cho phép frontend gọi API
-      credentials: true, // Quan trọng: Cho phép gửi cookie kèm request
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Các phương thức API được phép
-      allowedHeaders: "Content-Type,Authorization, token", // Cho phép headers cần thiết
-    })
-  );
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb' }));
